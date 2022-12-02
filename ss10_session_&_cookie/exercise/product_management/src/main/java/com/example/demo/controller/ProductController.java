f6374820927ea83f063c53e4f0792ff7c21567dc
TANNGUYENNGOC <nguyentantai1610@gmail.com>
2022-12-02 17:08:08 +0700

    update exercise

---
+++ ss10_session_&_cookie/exercise/product_management/src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@SessionAttributes("cart")
+@RequestMapping("/shop")
+public class ProductController {
+
+}
